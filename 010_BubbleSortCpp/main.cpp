808d58d992fd6e9a369abae91f2b877f3fe48d71
Thomas G <github@t-jay.net>
2017-05-22 16:05:37 +0200

    added bubble sort cpp

---
+++ 010_BubbleSortCpp/main.cpp
@@ -0,0 +1,46 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for (int a_i = 0; a_i < n; a_i++) {
+		cin >> a[a_i];
+	}
+
+	int numberOfAllSwaps = 0;
+	for (int i = 0; i < n; i++) 
+	{
+		// Track number of elements swapped during a single array traversal
+		int numberOfSwaps = 0;
+
+		for (int j = 0; j < n - 1; j++) {
+			// Swap adjacent elements if they are in decreasing order
+			if (a[j] > a[j + 1]) {
+				swap(a[j], a[j + 1]);
+				numberOfSwaps++;
+			}
+		}
+
+		numberOfAllSwaps += numberOfSwaps;
+
+		// If no elements were swapped during a traversal, array is sorted
+		if (numberOfSwaps == 0) {
+			break;
+		}
+	}
+
+	std::cout << "Array is sorted in " << numberOfAllSwaps << " swaps." << std::endl;
+
+	std::cout << "First Element: " << a[0] << std::endl;
+
+	std::cout << "Last Element: " << a[n-1] << std::endl;
+
+	return 0;
+}
