adc48af8776c97821f163e279fc6b316ecfa2bfb
Thomas G <github@t-jay.net>
2017-06-07 10:30:43 +0200

    added balanced brackets cs

---
+++ 006_BalancedBracketsCs/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Solution
+{
+  static bool isBalanced(String expression)
+  {
+    Stack<char> s = new Stack<char>();
+    foreach (char c in expression)
+    {
+      if (c == '{') s.Push('}');
+      else if (c == '[') s.Push(']');
+      else if (c == '(') s.Push(')');
+      else
+      {
+        if (s.Count == 0 || c != s.Peek())
+          return false;
+        s.Pop();
+      }
+    }
+    return s.Count == 0;
+  }
+
+  static void Main(String[] args)
+  {
+    int t = Convert.ToInt32(Console.ReadLine());
+    for (int a0 = 0; a0 < t; a0++)
+    {
+      string expression = Console.ReadLine();
+      if (isBalanced(expression))
+      {
+        Console.WriteLine("YES");
+      }
+      else
+      {
+        Console.WriteLine("NO");
+      }
+    }
+  }
+}
